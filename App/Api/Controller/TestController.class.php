0d28b4cc10bb12fd44b89f9c2f71492fbfc78a17
root <root@jim-notebook.workgroup>
2015-01-05 20:30:14 +0800

    new file:   App/Api/Controller/TestController.class.php

---
+++ App/Api/Controller/TestController.class.php
@@ -0,0 +1,7 @@
+<?php
+namespace api\Controller;
+use Api\Controller;
+include_once(dirname(__FILE__).'/BaseController.class.php');
+class TestController extends BaseController {
+	protected $_module_name = 'test';
+}
