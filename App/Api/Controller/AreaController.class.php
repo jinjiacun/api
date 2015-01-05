d62795c2b1502d3dda7df85b71bb449876b7d8c7
jinjiacun <jinjiacuncucu@sina.com>
2015-01-05 16:19:30 +0800

    modified:   App/Api/Controller/AreaController.class.php

--- App/Api/Controller/AreaController.class.php
+++ App/Api/Controller/AreaController.class.php
@@ -18,6 +18,34 @@ class AreaController extends BaseController {
     protected $arrchildid;
     protected $listorder;    
 
+	public function get_list($content)
+	{
+		list($data, $record_count) = parent::get_list($content);
+
+		$list = array();
+		if($data)
+		{
+			foreach($data as $v)
+			{
+				$list[] = array(
+						'areaid'      =>intval($v['areaid']),
+						'areaname'    =>urlencode($v['name']),
+						'parentid'    =>intval($v['parentid']),
+						'arrparentid' =>$v['arrparentid'],
+						'child'       =>intval($v['child']),
+						'arrchildid'  =>urlencode($v['arrchildid']),
+						'listorder'   =>intval($v['listorder']),
+					);	
+			}
+			unset($data, $v);
+		}
+
+		return array(200, array(
+							'list'=>$list,
+							'record_count'=>$record_count)
+					);
+	}
+
 	public function add($content)
 	{}
 	
