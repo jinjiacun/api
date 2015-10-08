38dc17d7ccbacf089205a162b3511ca6f7d1ca86
jinjiacun <jinjiacuncucu@sina.com>
2015-10-08 11:12:23 +0800

    modified:   App/Soapi/Controller/BaseController.class.php

--- App/Soapi/Controller/BaseController.class.php
+++ App/Soapi/Controller/BaseController.class.php
@@ -513,7 +513,7 @@ class BaseController extends Controller {
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1); 
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER,FALSE); 
 		curl_setopt($ch, CURLOPT_HTTPGET, true); 
-		curl_setopt($ch, CURLOPT_TIMEOUT, 30); 
+		curl_setopt($ch, CURLOPT_TIMEOUT, 5); 
 		if($params !== false){
 		 	curl_setopt($ch, CURLOPT_POSTFIELDS , $params);
 		} 
@@ -521,8 +521,19 @@ class BaseController extends Controller {
 		curl_setopt($ch, CURLOPT_URL,$url); 
 		curl_setopt($ch, CURLOPT_HTTPHEADER, $header); 
 		$result = curl_exec($ch); 
+		$curl_errno = curl_errno($ch); 
+        $curl_error = curl_error($ch);
 		curl_close($ch); 
-		 
+		if($curl_errno >0){ 
+                echo "cURL Error ($curl_errno): $curl_error\n"; 
+                $result = array();
+        }
+        /*
+        else{ 
+                echo "Data received: $data\n"; 
+        }*/  
+
+
 		return $result; 
 	}
 	
