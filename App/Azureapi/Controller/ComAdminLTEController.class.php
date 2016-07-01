7760e8a7c0568f90e805b7e005c2f5cbaf6f3491
jinjiacun123 <jinjiacuncucu@sina.com>
2016-07-01 20:31:14 +0800

    新文件:       ComAdminLTEController.class.php

---
+++ App/Azureapi/Controller/ComAdminLTEController.class.php
@@ -0,0 +1,63 @@
+<?php
+namespace Azureapi\Controller;
+use Azureapi\Controller;
+include_once(dirname(__FILE__).'/BaseController.class.php');
+/**
+--用户诊断管理--
+------------------------------------------------------------
+function of api:
+------------------------------------------------------------
+*/
+class ComAdminLTEController extends BaseController {
+  /**
+   * sql script:
+    create table sp_com_admin_lte(ComAdminId int primary key auto_increment,
+                            ComTLevel varchar(50),
+                            ComTStyle varchar(200),
+                            ComTIntro text,
+                            ComLiveNum int,
+                            ComFanNum int,
+                            ComInterNum int
+                 )charset=utf8;
+   * */
+
+   protected $_module_name = 'com_admin_lte';
+   protected $_key = 'ComAdminId';
+
+   protected $ComAdminId;
+   protected $ComTLevel;
+   protected $ComTStyle;
+   protected $ComTIntro;
+   protected $ComLiveNum;
+   protected $ComFanNum;
+   protected $ComInterNum;
+
+   public function get_list()
+   {
+       list($data, $record_count) = parent::get_list($content);
+       $list = array();
+       if($data)
+       {
+           $list[] = array(
+               'ComAdminId' => $v['ComAdminId'],
+               'ComTLevel' => $v['ComTLevel'],
+               'ComTStyle' => $v['ComTStyle'],
+               'ComTIntro' => $v['ComTINtro'],
+               'ComLiveNum' => $v['ComLiveNum'],
+               'ComFanNum' => $v['ComFanNum'],
+               'ComIntroNum' => $v['ComIntroNum'],
+           );
+       }
+
+       return array(200,
+       array(
+           'list'=>$list,
+           'record_count'=>$record_count,
+       )
+       );
+   }
+   
+   //通过关键字ComAdminId
+   public function get_info_by_key($contentx){
+
+   }
